6f531203a16df2d1253ab29f6e59c72a207b3bba
Natasha <127nbonilla@frhsd.com>
2025-12-15 14:03:33 -0500

    joselin likes pasta

---
+++ script.js
@@ -0,0 +1,11 @@
+let sumbit= document.getElementById("button");
+console.log(button);
+
+sumbit.addEventListener('click',getWords);
+
+function getWords(){
+console.log(sumbit);
+
+}
+
+var words= document.querySelectorAll('input[type=text]');
