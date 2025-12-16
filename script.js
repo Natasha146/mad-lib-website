51c64004344ef39fa1bf19c3340759d9154b0a7e
Natasha <127nbonilla@frhsd.com>
2025-12-16 14:03:28 -0500

    pizza

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ sumbit.addEventListener('click',getWords);
 function getWords(){
 console.log(sumbit);
 
-}
-
 var words= document.querySelectorAll('input[type=text]');
+
+var placeholders= document.querySelectorAll('.userInput');
+console.log(placeholders)
+
+placeholders.forEach((placeholders, index)=> {
+    console.log(placeholders, index);
+});
+
+console.log(2)
+
+}
